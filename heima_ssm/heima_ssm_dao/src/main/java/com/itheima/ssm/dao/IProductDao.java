0b3bdbb15a92e38205220036e0e102a99821cf8b
xt <2512206586@qq.com>
2019-03-27 22:08:53 +0800

    3月27日合入

--- heima_ssm/heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IProductDao.java
+++ heima_ssm/heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IProductDao.java
@@ -1,10 +1,13 @@
 package com.itheima.ssm.dao;
 
 import com.itheima.ssm.domain.Product;
+import org.apache.ibatis.annotations.Select;
+
 import java.util.List;
 
 public interface IProductDao {
 
     //查询所有的产品信息
+    @Select("select * from product")
     public List<Product> findAll() throws Exception;
 }
